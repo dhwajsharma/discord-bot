24fb3dc6708b856d9c9800350174859116d21653
Dhwaj <54977261+dhwajsharma@users.noreply.github.com>
2020-12-29 20:12:33 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -14,26 +14,26 @@ function readyDiscord() {
 client.on('message', gotMessage);
 
 async function gotMessage(msg) {
-    if (msg.channel.id == '793447643674181632') {
 
-        let tokens = msg.content.split(' ');
 
-        if (tokens[0] === '!badmaash') {
-            const index = Math.floor(Math.random() * replies.length);
-            msg.channel.send(replies[index]);
-        } else if (tokens[0] == '!gif') {
+    let tokens = msg.content.split(' ');
 
-            let keywords = 'doge';
-            if (tokens.length > 1) {
-                keywords = tokens.slice(1, tokens.length).join(" ");
-            }
+    if (tokens[0] === '!badmaash') {
+        const index = Math.floor(Math.random() * replies.length);
+        msg.channel.send(replies[index]);
+    } else if (tokens[0] == '!gif') {
 
-            let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}&ContentFilter=low`;
-            let response = await fetch(url);
-            let json = await response.json();
-            console.log(json);
-            let index = Math.floor(Math.random() * json.results.length);
-            msg.channel.send(json.results[index].url);
+        let keywords = 'doge';
+        if (tokens.length > 1) {
+            keywords = tokens.slice(1, tokens.length).join(" ");
         }
+
+        let url = `https://api.tenor.com/v1/search?q=${keywords}&key=${process.env.TENORKEY}&ContentFilter=low`;
+        let response = await fetch(url);
+        let json = await response.json();
+        console.log(json);
+        let index = Math.floor(Math.random() * json.results.length);
+        msg.channel.send(json.results[index].url);
     }
+
 }
